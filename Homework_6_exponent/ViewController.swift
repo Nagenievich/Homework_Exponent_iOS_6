b20d2757f1158ffbbb858c0aa836adaf8f2a9a58
Alexey Makarov <alexeynkrv@gmail.com>
2021-02-06 06:12:26 +0700

    Initial Commit

---
+++ Homework_6_exponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_6_exponent
+//
+//  Created by Alexey Makarov on 06.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
